ef27967aac76412f0d4b47af3441354fa8ea42fe
yasar1995 <39369190+yasar1995@users.noreply.github.com>
2018-11-15 12:43:37 +0530

    Update Pass_Argument.py

--- Pass_Argument.py
+++ Pass_Argument.py
@@ -1,5 +1,19 @@
-#!/usr/bin/python
-import sys
-filename = sys.argv[1]
-print(filename)
-echo "x + y = "
+while True:
+    try:
+        userInput = raw_input("Would you like to 'upgrade' or 'downgrade' the image: ")
+        userInput = userInput.lower();
+    except ValueError:
+        print("Sorry, I didn't understand that.")
+        continue
+
+    if userInput not in ['upgrade', 'downgrade']:
+        print("Sorry, your response must be either 'upgrade' or 'downgrade'.")
+        continue
+    else:
+        #age was successfully parsed, and we're happy with its value.
+        #we're ready to exit the loop.
+        break
+if userInput == "upgrade": 
+    print("You are upgraded")
+elif userInput == "downgrade":
+    print("You are downgraded")
