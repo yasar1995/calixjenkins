db0d575107f59c369de65a7059a3cb6cbe3aa04f
yasar1995 <39369190+yasar1995@users.noreply.github.com>
2018-11-15 11:08:56 +0530

    Add files via upload

---
+++ UbuntuReboot.java
@@ -0,0 +1,53 @@
+import java.io.*;
+public class UbuntuReboot
+{
+	public static void main(String[] args) throws Exception 
+	{
+	    String[] cmd = {"sudo","-S", "python", "OriginalFlashBackup.py"};
+	    System.out.println(runSudoCommand(cmd));
+	}
+
+	  private static int runSudoCommand(String[] command) throws Exception 
+	{
+	    Runtime runtime =Runtime.getRuntime();
+	    Process process = runtime.exec(command);
+	    OutputStream os = process.getOutputStream();
+	    os.write("Y@$@r1995\n".getBytes());
+	    os.flush();
+	    os.close();
+	    process.waitFor();
+	    System.out.println("Writing Sudo Password");
+	    String output = readFile(process.getInputStream());
+	    if (output != null && !output.isEmpty()) {
+	      System.out.println(output);
+	    }
+	    String error = readFile(process.getErrorStream());
+	    if (error != null && !error.isEmpty()) {
+	      System.out.println(error);
+	    }
+	    return process.exitValue();
+	 }
+
+	  private static String readFile(InputStream inputStream) throws Exception 
+	{
+	    if (inputStream == null) {
+	      return "";
+	    }
+	    StringBuilder sb = new StringBuilder();
+	    BufferedReader bufferedReader = null;
+	    try {
+	      bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+	      String line = bufferedReader.readLine();
+	      while (line != null) {
+		sb.append(line);
+		line = bufferedReader.readLine();
+	      }
+	      return sb.toString();
+	    } finally {
+	      if (bufferedReader != null) {
+		bufferedReader.close();
+	      }
+	    }
+	  }
+}
+
